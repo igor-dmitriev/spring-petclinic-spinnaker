cd3d58fcbac662077ad75ebffc38eaea9486a7c7
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2020-10-29 18:56:49 +0200

    Welcome SPD

--- frontend/src/components/WelcomePage.tsx
+++ frontend/src/components/WelcomePage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export default () => (
   <span>
-    <h2>Welcome</h2>
+    <h2>Welcome SPD</h2>
     <div className='row'>
       <div className='col-md-12'>
         <img className='img-responsive' src='/images/pets.png' />
